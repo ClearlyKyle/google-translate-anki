4f96c5a1c07238ced9bd9afdb79bd1f43088d326
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2021-05-17 17:27:36 +0100

    added margin to button

--- chrome/bubble_compiled.js
+++ chrome/bubble_compiled.js
@@ -4218,7 +4218,7 @@
           b += "auto" != f ? '<option value="' + gg(f[0]) + '"' + (f[0] == a.ec ? " selected" : "") + ">" + gg(f[1]) + "</option>" : ""
         }
         /* ANKI BUTTON */
-        b += '</select><button class="btnAnki">ANKI</button></div><div class="gtx-source-audio"><div class="jfk-button-img"></div></div><div class="gtx-body">' + 
+        b += '</select><button class="btnAnki" style="margin-left:10px">ANKI</button></div><div class="gtx-source-audio"><div class="jfk-button-img"></div></div><div class="gtx-body">' + 
                   gg(a.query) + '</div><br><div class="gtx-language">' + 
                   gg(a.hc) + '</div><div class="gtx-target-audio"><div class="jfk-button-img"></div></div><div class="gtx-body gtx-target" data-source="' + gg(a.query) + '">' +
                   gg(a.Ob) + "</div>";
